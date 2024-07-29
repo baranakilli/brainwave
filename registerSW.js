bdf7f3f94fbb4dafbbe9fab44d3c047517298a85
Baran Akıllı <akillibaran@hotmail.com>
2024-07-29 11:58:48 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/brainwave/sw.js', { scope: '/brainwave/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
