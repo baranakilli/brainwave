d3a997351f85a6612a96e43c132a96ecc6c17b23
Baran Akıllı <akillibaran@hotmail.com>
2024-07-29 14:41:02 +0300

    fixed pwa issues

--- vite.config.js
+++ vite.config.js
@@ -2,54 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const projectName = "brainwave";
-
 export default defineConfig({
-  base: `/${projectName}/`,
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "autoUpdate", // Kullanıcı müdahalesi olmadan güncellemeleri otomatik uygular
       devOptions: {
         enabled: true,
       },
       workbox: {
-        skipWaiting: true,
-        clientsClaim: true,
-      },
-      manifest: {
-        short_name: "Brainwave",
-        name: "Brainwave App",
-        icons: [
-          {
-            src: `/${projectName}/android-chrome-192x192.png`,
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: `/${projectName}/android-chrome-512x512.png`,
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: `/${projectName}/android-chrome-512x512.png`,
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: `/${projectName}/android-chrome-512x512.png`,
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-        start_url: `/${projectName}/`,
-        display: "standalone",
-        scope: `/${projectName}/`,
-        theme_color: "#0E0C15",
-        background_color: "#ffffff",
+        skipWaiting: true, // Yeni servis çalışanı hemen etkinleşir
+        clientsClaim: true, // Yeni servis çalışanı hemen kontrolü devralır
       },
     }),
   ],
+  base: "/brainwave/",
 });
